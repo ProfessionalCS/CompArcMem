aa679c9dace198bcb5f25f6196b5011cd0a5df03
Jorge <jorgegarcia_tx@outlook.com>
2026-03-09 23:09:40 -0500

    Renaming and stuff, prep for miss handling

--- MemorySystem/cacheDataTypes.sv
+++ MemorySystem/cacheDataTypes.sv
@@ -1,24 +1,38 @@
 `timescale 1ns/1ps
 
 package cacheDataTypes;
-	localparam int L2_CAPACITY = 4096;
+	// Common info for all cache levels
 	localparam int BLOCK_SIZE = 64;
+	localparam int DATA_WIDTH = 512;
+	localparam int OFFSET_WIDTH = $clog2(BLOCK_SIZE);
+
+	// Info for L1 goes here:
+
+	// Info for L2
+	localparam int L2_CAPACITY = 4096;
 	localparam int L2_WAYS = 4;
 	localparam int L2_SETS = 16;
 	localparam int L2_MSHR_COUNT = 4;
 	localparam int L2_WAY_WIDTH = $clog2(L2_WAYS);
-	localparam int L2_ADDR_WIDTH = 30; // 1 GB address space
-	localparam int DATA_WIDTH = 512;
-	localparam int OFFSET_WIDTH = $clog2(BLOCK_SIZE);
+	localparam int L2_ADDR_WIDTH = 30; // 1 GB address space (physical)
 	localparam int L2_INDEX_WIDTH = $clog2(L2_SETS);
 	localparam int L2_TAG_WIDTH = L2_ADDR_WIDTH - OFFSET_WIDTH - L2_INDEX_WIDTH;
+	localparam int L2_MSHR_QUEUE_SIZE = 8;
 
-	// typedef struct packed {
-	// 	logic valid;
-	// 	logic [ADDR_WIDTH-1:0] blockAddr;
-	// 	logic isWrite;
-	// 	logic [DATA_WIDTH-1:0] wdata;
-	// } mshrEntry;
+
+
+	/** L2 structures and stuff **/
+
+	typedef struct packed {
+		logic isWrite;
+		logic [DATA_WIDTH-1:0] writeData; // Data to write for store, don't care for load
+	} l2_mshr_miss_t;
+
+	typedef struct packed {
+		logic valid;
+		logic [L2_ADDR_WIDTH-1:0] blockAddr;
+		logic[$clog2(L2_MSHR_QUEUE_SIZE)-1:0] tail;
+	} l2_mshr_t;
 
 	typedef struct packed {
 		logic [L2_TAG_WIDTH-1:0] tag;
--- MemorySystem/llcd.sv
+++ MemorySystem/llcd.sv
@@ -2,7 +2,7 @@
 
 import cacheDataTypes::*;
 
-// 4 KiB capacity, 64 B block size, 4-way set associative, 16 sets
+// 4 KiB capacity, 64 B block size, 4-way set associative, 16 L2_sets
 module llcd(
 	input logic clk,
 	input logic rstN,
@@ -11,7 +11,7 @@ module llcd(
 	/** Interface to L1 cache **/
 	input logic l1ReqValid, // l2_req_valid from l1
 	input logic l1ReqWrite, // 1=write request, 0=read request
-	input logic [ADDR_WIDTH-1:0] l1Addr, // l2_req_addr from l1, address sent by l1
+	input logic [L2_ADDR_WIDTH-1:0] l1Addr, // l2_req_addr from l1, address sent by l1
 	input logic [DATA_WIDTH-1:0] l1DataIn,
 	output logic [DATA_WIDTH-1:0] l1DataOut, // l2_resp_data from l1, data going into l1
 	output logic l1RespValid, // l2_resp_valid from l1
@@ -21,7 +21,7 @@ module llcd(
 	input logic memReadRespValid,
 	input logic memWriteReqReady,
 	input logic [DATA_WIDTH-1:0] memDataIn, // Data read from memory
-	output logic [ADDR_WIDTH-1:0] memAddr,
+	output logic [L2_ADDR_WIDTH-1:0] memAddr,
 	output logic [DATA_WIDTH-1:0] memDataOut, // Data to write to memory
 	output logic memReadReqValid,
 	output logic memReadRespReady,
@@ -29,32 +29,33 @@ module llcd(
 );
 
 
-	logic [DATA_WIDTH-1:0] dataArray [SETS-1:0][WAYS-1:0];
-	l2LineMetadata lineMd [SETS-1:0][WAYS-1:0];
+	logic [DATA_WIDTH-1:0] dataArray [L2_SETS-1:0][L2_WAYS-1:0];
+	l2LineMetadata lineMd [L2_SETS-1:0][L2_WAYS-1:0];
 
 	// Using PLRU for replacement
 	// Need A-1 bits per set
-	logic [WAYS-2:0] plruBits [SETS-1:0];
+	logic [L2_WAYS-2:0] plruBits [L2_SETS-1:0];
 
-	// mshrEntry mshrEntries [MSHR_COUNT-1:0];
+	l2_mshr_t mshr[L2_MSHR_COUNT];
+	l2_mshr_miss_t missQueues[L2_MSHR_COUNT][L2_MSHR_QUEUE_SIZE];
 
 	// Extract index and tag from the address
-	logic [INDEX_WIDTH-1:0] index;
-	logic [TAG_WIDTH-1:0] tag;
+	logic [L2_INDEX_WIDTH-1:0] index;
+	logic [L2_TAG_WIDTH-1:0] tag;
 	logic [OFFSET_WIDTH-1:0] blockOffset;
 	always_comb begin
-		index = l1Addr[OFFSET_WIDTH +: INDEX_WIDTH];
-		tag = l1Addr[OFFSET_WIDTH + INDEX_WIDTH +: TAG_WIDTH];
+		index = l1Addr[OFFSET_WIDTH +: L2_INDEX_WIDTH];
+		tag = l1Addr[OFFSET_WIDTH + L2_INDEX_WIDTH +: L2_TAG_WIDTH];
 		blockOffset = l1Addr[0 +: OFFSET_WIDTH];
 	end
 
 
 	// Tag matching
-	logic [WAYS-1:0] hitVec; // Vector indicating which way hits (if any)
+	logic [L2_WAYS-1:0] hitVec; // Vector indicating which way hits (if any)
 	logic cacheHit;
-	logic [WAY_WIDTH-1:0] hitWay; // Encoded way index of the hit
+	logic [L2_WAY_WIDTH-1:0] hitWay; // Encoded way index of the hit
 	always_comb begin
-		for (int w = 0; w < WAYS; w++) begin
+		for (int w = 0; w < L2_WAYS; w++) begin
 			hitVec[w] = lineMd[index][w].valid && (lineMd[index][w].tag == tag);
 		end
 	end
@@ -62,26 +63,13 @@ module llcd(
 	assign cacheHit = |hitVec;
 	always_comb begin
 		hitWay = '0;
-		for (int w = 0; w < WAYS; w++) begin
-			if (hitVec[w]) hitWay = WAY_WIDTH'(w);
+		for (int w = 0; w < L2_WAYS; w++) begin
+			if (hitVec[w]) hitWay = L2_WAY_WIDTH'(w);
 		end
 	end
 
 	// Miss/MSHR stuff, todo later
-	// logic missDetected;
-	// logic mshrFree;
-	// logic [WAY_WIDTH-1:0] mshrFreeIdx;
-	// always_comb begin
-	// 	missDetected = l1ReqValid && !cacheHit;
-	// 	mshrFree = 1'b0;
-	// 	mshrFreeIdx = '0;
-	// 	for (int m = 0; m < MSHR_COUNT; m++) begin
-	// 		if (!mshrEntries[m].valid && !mshrFree) begin
-	// 			mshrFree = 1'b1;
-	// 			mshrFreeIdx = WAY_WIDTH'(m);
-	// 		end
-	// 	end
-	// end
+
 
 	// Herein assume a hit was made
 
@@ -97,11 +85,11 @@ module llcd(
 
 				// Update PLRU bits for this set
 				if (hitWay < 2) begin
-					plruBits[index][0] <= (hitWay == 0) ? 1'b1 : 1'b0; // If we hit way0, set bit0 to 1, else set to 0
+					plruBits[index][0] <= (hitWay == 0) ? 1'b1 : 1'b0;
 				end else begin
-					plruBits[index][1] <= (hitWay == 2) ? 1'b1 : 1'b0; // If we hit way2, set bit1 to 1, else set to 0
+					plruBits[index][1] <= (hitWay == 2) ? 1'b1 : 1'b0;
 				end
-				plruBits[index][2] <= (hitWay < 2) ? 1'b1 : 1'b0; // If we hit in the upper half (way0 or way2), set bit2 to 1, else set to 0
+				plruBits[index][2] <= (hitWay < 2) ? 1'b1 : 1'b0;
 
 			end else begin
 				l1DataOut <= '0;
@@ -143,15 +131,3 @@ module llcd(
 
 
 endmodule: llcd /* verilator lint_off EOFNEWLINE */
-
-// TODO: Ignore MSHR for now
-// module mshrQueue #(
-// 	parameter QUEUE_SIZE = 4
-// )(
-// 	input logic clk,
-// 	input logic rstN,
-
-
-// );
-
-// endmodule: mshrQueue
